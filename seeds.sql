174b50cb0fdcb9248125fd31f749ef7e0e70ef52
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-04-30 17:32:55 -0700

    After 1st tutor

---
+++ seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES  ("Sales"),
+        ("Legal Dept"),
+        ("Finance"),
+        ("Engineering");
+        
+INSERT INTO roles (title, salary, department_id)
+VALUES  ("Engineer", "100000", 4),
+        ("Sales", "90000", 1),
+        ("Account", "80000", 3),
+        ("Legal", "70000", 2);
+        
+       
+INSERT INTO employee (first_name, last_name)
+VALUES  ("Jeff", "Schmidt"),
+        ("Tosh", "Smith"),
+        ("Brenda", "Johnson"),
+        ("Sarah", "Connor"),
+        ("Ashley", "Mcguile"),
+        ("Mike", "Jones");
